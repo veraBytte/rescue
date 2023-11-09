b0c10b227c53422fda8e6c8df3fe7dab9c0a03cd
Frank <fobellofrank@gmail.com>
2023-11-09 16:02:24 -0300

    Creacion de cards

---
+++ src/components/publicaciones/Cards.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+export default function Cards(props){
+    return (
+      <Card sx={(theme) =>({width:340 , bgcolor:'primary.main', })}>
+      <CardActionArea>
+        <Typography 
+          gutterBottom 
+          variant="h4" 
+          component="div" 
+          fontSize={16} 
+          fontWeight={700} 
+          textAlign={'center'}>
+            {props.title}
+        </Typography>
+        <CardMedia
+          sx={{
+            borderRadius:'16px',
+            mb:2
+          }}
+          component="img"
+          height="140"
+          image={props.img}
+          alt=""
+        />
+        <CardContent sx={{p:0}}>
+          <Typography variant="body1" color="text.secondary">
+            {props.date}
+          </Typography>
+          <Typography variant="body1" color="text.secondary">
+            {props.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions 
+      sx={{justifyContent:'center'}}>
+        <Button size="small" color="primary">
+        Ver más
+        </Button>
+      </CardActions>
+    </Card>
+    );
+}
--- src/components/publicaciones/Publicaciones.jsx
+++ src/components/publicaciones/Publicaciones.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import Cards from './Cards'
+import '../../styles/Publicaciones.css'
 export default function Publicaciones() {
+  const [publicacion, setPublicacion] = useState([
+    {
+      title : 'Inversiones Éticas: Más que ganancias',
+      img : 'img/cardImg.jpg',
+      date: '17/04/2023',
+      description : 'Las decisiones financieras han trascendido la mera maximización del rendimiento. Actualmente, muchos inversores desean que sus decisiones reflejen sus valores éticos y morales, dando lugar a las inversiones éticas o sostenibles.',
+    },
+    {
+      title : 'Inversiones Éticas: Más que ganancias',
+      img : 'img/cardImg.jpg',
+      date: '17/04/2023',
+      description : 'Las decisiones financieras han trascendido la mera maximización del rendimiento. Actualmente, muchos inversores desean que sus decisiones reflejen sus valores éticos y morales, dando lugar a las inversiones éticas o sostenibles.',
+    },
+    {
+      title : 'Inversiones Éticas: Más que ganancias',
+      img : 'img/cardImg.jpg',
+      date: '17/04/2023',
+      description : 'Las decisiones financieras han trascendido la mera maximización del rendimiento. Actualmente, muchos inversores desean que sus decisiones reflejen sus valores éticos y morales, dando lugar a las inversiones éticas o sostenibles.',
+    },
+  ])
+
   return (
-    <div>Aca van las Publicaciones</div>
+    <div className='cardContainer'>
+      {publicacion.map((publi, index) => {
+        return <Cards key={index} title={publi.title} img={publi.img} date={publi.date} description={publi.description}/>
+          
+      })}
+    </div>
+      
   )
 }
