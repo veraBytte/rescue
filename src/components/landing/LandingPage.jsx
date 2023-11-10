63bc5331eb0ba7a0575e327e3a66b57d5bef6fa4
cubo1991 <adavidlopezmathez@gmail.com>
2023-11-10 12:32:20 -0300

    Arreglos

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -1,53 +1,75 @@
 import React from 'react';
-import { Box, Card, CardContent, CardMedia, Container, Grid, Typography, Input, TextField } from '@mui/material';
-import  heroJPG from '../../assets/monedasHierba.jpg'
+import { Box, Card, CardContent, CardMedia, Container, Grid, Typography, TextField } from '@mui/material';
 
+import  heroJPG from '../../assets/monedasHierba.jpg'
 
 function LandingPage() {
  
+ 
   return (
     <Container>
-      <Box sx={{ my: 4,      
-      }}>
+      <Box sx={{ my: 4 }}>
       
-      <Box sx={{ 
-  my: 4,
-  backgroundImage: `url(${heroJPG})`,
-  backgroundRepeat:"no-repeat",
-  width: window.innerWidth <= 768 ? '360px' : '100%',
-  height: window.innerWidth <= 768 ? '488px' : 'auto',
-  backgroundSize:'contain',
-  backgroundPosition:'center',
-  position: 'relative',
+     
+  <Box 
+    sx={{ 
+        width: '360px', 
+        height: '488px', 
+        position: 'absolute'
+    }}
+>
+    <img   src={heroJPG} 
+        alt="Descripción de la imagen" 
+        sx={{ 
+            position: 'absolute', 
+            top: '57px',
+            width: '360px', 
+            height: '488px'
+        }} 
+    />
+    <Box 
+        sx={{ 
+            backgroundColor: 'rgba(0, 0, 0, 0.7)', 
+            position: 'absolute', 
+            top: '0',
+            width: '100%', 
+            height: '100%'
+        }}
+    />
+</Box>
+          <Box >
+          <TextField 
+  sx={{
+    width: 328,
+    height: 56,
+    position: 'fixed',
+    top: 80,
+    left: 16,
+     padding: '0px 16px',
+    ".MuiOutlinedInput-root": {
+      borderRadius: '100px',
+    }
+  }} 
+  id="outlined-basic" 
+  label="Outlined" 
+  variant='outlined'
+/>
 
-  '::after': {
-    content: '""',
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    backgroundColor: 'rgba(9, 9, 9, 0.7)',
-  },
-}}>
+          </Box>
 
-
-          <box sx={{m:2}}>
-          <TextField id="outlined-basic" label= "Outlined" variant='outlined'/>
-          </box>
-
-       <Box
-       >
+    
        
-         <Typography sx={{ typography: 'titulos', position:'absolute', left:16, top: 176, mb:1, zIndex: 1 }} component="h2">FINANCIAMIENTO SOSTENIBLE</Typography>
+      <Typography sx={{ typography: 'titulos', position:'absolute', left: 16, top: 176}} component="h2">FINANCIAMIENTO SOSTENIBLE</Typography>
+            
+
        
-        <box>
-         <Typography sx={{typography: 'parrafos', width:'240px', height:264, top:208, left:16,  position:'absolute', zIndex: 1}}> Impulsamos el desarrollo de finanzas de impacto, liderando la transición hacia un modelo financiero sostenible</Typography>
-        </box>
+        <Box>
+         <Typography sx={{typography: 'parrafos', width:'240px', height:264, top:208, left:16,  position:'absolute', }}> Impulsamos el desarrollo de finanzas de impacto, liderando la transición hacia un modelo financiero sostenible</Typography>
         </Box>
+       
          
          
-        </Box>
+       
 
         {/* Tarjetas de Información */}
         <Grid container spacing={3}>
@@ -67,6 +89,7 @@ function LandingPage() {
                   <Typography variant="body2" color="text.secondary">
                     Descripción detallada del contenido de esta tarjeta. Puede variar según el propósito de la tarjeta.
                   </Typography>
+                  
                 </CardContent>
               </Card>
             </Grid>
